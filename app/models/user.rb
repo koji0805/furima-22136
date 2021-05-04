bc5d122cf7515f99300b57051b1871a16636d5d3
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-04 16:51:45 +0900

    仮テストコード記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         with_options presence: true do
-         validates :nickname
-         with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' Full-width characters' } do
-         validates :first_name_kana, presence: true,format:{with: /\A[ｧ-ﾝﾞﾟ]+\z/}
-         validates :last_name_kana, presence: true,format:{with: /\A[ｧ-ﾝﾞﾟ]+\z/}
-         validates :birthday, presence: true
-         end
+    with_options presence: true do
+    validates :nickname, presence: true, (:password: "min_alpha: 1, min_numric: 1")
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' Full-width characters' } do
+    validates :first_name
+    validates :last_name
+    end
+
+    with_options format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: ' Full-width characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+    end
+    validates :birthday, presence: true
+    end
 end
