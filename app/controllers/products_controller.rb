b92d0f9ad263dbc2c924506c639025818c5256bb
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-04 21:42:55 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  private
+
+  def products_params
+    params.require(:products).permit(:concept, :image).merge(user_id: current_user.id)
+  end
 end
