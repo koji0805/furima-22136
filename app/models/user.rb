d009e6637906b2b8971b890cdaf559e176069cbe
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-04 13:12:18 +0900

    テストコード　app/models/user_spec.rb修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,6 @@ class User < ApplicationRecord
          with_options presence: true do
          validates :nickname
          with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' Full-width characters' } do
-         validates :first_name, presence: true,format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :last_name, presence: true,format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :first_name_kana, presence: true,format:{with: /\A[ｧ-ﾝﾞﾟ]+\z/}
-         validates :last_name_kana, presence: true,format:{with: /\A[ｧ-ﾝﾞﾟ]+\z/}
          validates :birthday, presence: true
          end
 end
