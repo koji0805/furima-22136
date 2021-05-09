2ee1e1d1b1b3b03d17c33c07b52a7dafbf57cc09
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-09 18:56:11 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,10 +20,23 @@ class ItemsController < ApplicationController
     def show
       @item = Item.find(params[:id])
     end
+      
+    def edit
+      @item = Item.find(params[:id])
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(params[:id])
+      else
+        render :edit
+      end
+    end
   
     private
   
     def item_params
       params.require(:item).permit(:name, :image, :description,:category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :image,).merge(user_id: current_user.id)
     end
-end
+  end
