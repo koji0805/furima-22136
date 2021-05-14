d92e60686781755186f9d57a35e5b2023428eb51
kojio0805 <koji.0805.fuji@gmail.com>
2021-05-14 18:14:01 +0900

    修正その６

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,9 +1,9 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :address1, :address2, :telephone, :building, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :address1, :address2, :telephone, :building, :item_id, :token
 
   with_options presence: true do
-    validates :address1, :address2, :token, :item_id
+    validates :address1, :address2, :token, :user_id,:item_id
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :telephone, format: { with: /\A\d{10,11}\z/, message: "can't be blank" }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
